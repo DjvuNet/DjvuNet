8b1d9c44847fce13815dd2553d282c48a7f83599
Jacek Blaszczynski <djvunet@outlook.com>
2017-04-03 10:27:04 +0200

    Refactor project structure and dependencies, remove reference to libdjvulibre temporarily to pass appveyor ci builds

--- DjvuNet.Shared.Tests/Util.cs
+++ DjvuNet.Shared.Tests/Util.cs
@@ -112,25 +112,6 @@ namespace DjvuNet.Tests
             Assert.True(false, info);
         }
 
-        public static void VerifyDjvuDocumentCtor(int pageCount, DjvuDocument document)
-        {
-            VerifyDjvuDocument(pageCount, document);
-            Assert.False(document.Disposed);
-            if (pageCount > 1)
-                Assert.NotNull(document.Directory);
-            Assert.NotNull(document.ActivePage);
-            Assert.NotNull(document.RootForm);
-            Assert.NotNull(document.Navigation);
-        }
-
-        public static void VerifyDjvuDocument(int pageCount, DjvuDocument document)
-        {
-            Assert.NotNull(document.FirstPage);
-            Assert.NotNull(document.LastPage);
-            if (pageCount > 0)
-                Assert.Equal<int>(pageCount, document.Pages.Length);
-        }
-
         public static string GetTestFilePathTemplate()
         {
             char dirSep = Path.DirectorySeparatorChar;
@@ -146,19 +127,5 @@ namespace DjvuNet.Tests
             string filePath = String.Format(filePathTempl, index);
             return filePath;
         }
-
-        public static DjvuDocument GetTestDocument(int index, out int pageCount)
-        {
-            pageCount = 0;
-
-            if (index < 1)
-                return null;
-
-            pageCount = GetTestDocumentPageCount(index);
-            return new DjvuDocument(GetTestFilePath(index));
-        }
-
-
-
     }
 }
