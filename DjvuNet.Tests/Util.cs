2321bb60146fa1b29dbc05225b1ae4a5238c7714
Jacek Blaszczynski <djvunet@outlook.com>
2017-04-03 10:27:04 +0200

    Refactor project structure and dependencies, remove reference to libdjvulibre temporarily to pass appveyor ci builds

---
+++ DjvuNet.Tests/Util.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DjvuNet.Tests
+{
+    public static partial class Util
+    {
+        public static DjvuDocument GetTestDocument(int index, out int pageCount)
+        {
+            pageCount = 0;
+
+            if (index < 1)
+                return null;
+
+            pageCount = GetTestDocumentPageCount(index);
+            return new DjvuDocument(GetTestFilePath(index));
+        }
+
+        public static void VerifyDjvuDocumentCtor(int pageCount, DjvuDocument document)
+        {
+            VerifyDjvuDocument(pageCount, document);
+            Assert.False(document.Disposed);
+            if (pageCount > 1)
+                Assert.NotNull(document.Directory);
+            Assert.NotNull(document.ActivePage);
+            Assert.NotNull(document.RootForm);
+            Assert.NotNull(document.Navigation);
+        }
+
+        public static void VerifyDjvuDocument(int pageCount, DjvuDocument document)
+        {
+            Assert.NotNull(document.FirstPage);
+            Assert.NotNull(document.LastPage);
+            if (pageCount > 0)
+                Assert.Equal<int>(pageCount, document.Pages.Length);
+        }
+
+    }
+}
