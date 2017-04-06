0ea8573d02d5bb24beb5739026ef295ab7f88135
Jacek Blaszczynski <djvunet@outlook.com>
2017-04-06 06:09:41 +0200

    Implement independent from CI vendor CI test infrastructure - WIP

---
+++ DjvuNet.Shared.Tests/TestExecution/ITestManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace DjvuNet.Tests
+{
+    public interface ITestManager
+    {
+        bool IsInitialized { get; }
+
+        TestFramework Framework { get; set; }
+
+        IReadOnlyList<string> TargetNames { get; }
+
+        int AfterBuildTests();
+
+        int AfterRunTests();
+
+        int BeforeBuildTests();
+
+        int BeforeRunTests();
+
+        int BuildTests();
+
+        int RunTestFuzzer();
+
+        int RunTests(string target);
+    }
+
+    public enum TestFramework
+    {
+        Unknown = 0,
+        MSTest = 1,
+        xUnit = 2,
+        NUnit = 3,
+        MSpec = 4,
+    }
+}
