9289d421dd3b9ac186204a5dd5a8967f7bb101d5
Jacek Blaszczynski <djvunet@outlook.com>
2017-04-11 13:43:26 +0200

    Support incremental ''builds" for test artifacts

--- DjvuNet.Shared.Tests/UtilJson.cs
+++ DjvuNet.Shared.Tests/UtilJson.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 using DjvuNet.DjvuLibre;
@@ -58,6 +59,83 @@ namespace DjvuNet.Tests
 
 
                 return retVal;
+            }
+        }
+
+        /// <summary>
+        /// Build function used to determine if any build operations on test artifacts are required.
+        /// Used in ArtifactsBuildRequired Task in DjvuNet.Tests.targets build file.
+        /// </summary>
+        /// <param name="ArtifactsDirectory">
+        /// Root directory for test artifacts.
+        /// </param>
+        /// <param name="ArtifactsSearchPattern">
+        /// File search patterns for artifacts root directory. Individual patterns can be separated with semicolon.
+        /// </param>
+        /// <param name="SubdirectoryList">
+        /// List of subdirectories expressed as relative paths to artifacts directory separated by semicolon 
+        /// </param>
+        /// <param name="SubdirectorySearchPatterns">
+        /// List of subdirectory lists of search patterns. 
+        /// Each subdirectory list is separated with semicolon and individual directory lists are separated by |</param>
+        /// <param name="HashFilePath">
+        /// Path to file containing hashes and names with relative paths of all files tracked files in artifacts directory.
+        /// </param>
+        /// <param name="CalculateHashes">
+        /// If true hash file should be updated with new hashes at least for one file.
+        /// </param>
+        public static void ArtifactsBuildRequired(string ArtifactsDirectory, string ArtifactsSearchPattern, 
+            string SubdirectoryList, string SubdirectorySearchPatterns, string HashFilePath, out bool CalculateHashes)
+        {
+            FileInfo targetnfo = new FileInfo(HashFilePath);
+            if (!targetnfo.Exists)
+                CalculateHashes = true;
+            else
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(ArtifactsDirectory);
+                FileSystemInfo youngestOne = null;
+                if (dirInfo.Exists)
+                {
+                    string[] patterns = ArtifactsSearchPattern.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string s in patterns)
+                    {
+                        FileInfo artFile = dirInfo.GetFiles(s, SearchOption.TopDirectoryOnly)
+                            .OrderByDescending(file => file.LastWriteTimeUtc).FirstOrDefault();
+                        if (youngestOne == null || youngestOne.LastWriteTimeUtc < artFile.LastWriteTimeUtc)
+                            youngestOne = artFile;
+                    }
+                }
+
+                string[] subdirectories = SubdirectoryList.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                string[] subPatterns = SubdirectorySearchPatterns.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                string[][] subsubPatterns = new string[subPatterns.Length][];
+                for(int i = 0; i < subPatterns.Length; i++)
+                {
+                    string[] subsubP = subPatterns[i].Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+                    subsubPatterns[i] = subsubP;
+                }
+
+                for (int i = 0; i < subdirectories.Length; i++)
+                {
+                    string d = subdirectories[i];
+                        
+                    DirectoryInfo subdirInfo = new DirectoryInfo(Path.Combine(ArtifactsDirectory, d));
+                    if (subdirInfo.Exists)
+                    {
+                        string[] patterns = new string[] { "*" };
+                        if (i < subsubPatterns.Length)
+                            patterns = subsubPatterns[i];
+
+                        foreach (string pattern in patterns)
+                        {
+                            FileSystemInfo fInfo = subdirInfo.GetFiles(pattern, SearchOption.TopDirectoryOnly)
+                                .OrderByDescending(file => file.LastWriteTimeUtc).FirstOrDefault();
+                            if (youngestOne.LastWriteTimeUtc < fInfo.LastWriteTimeUtc)
+                                youngestOne = fInfo;
+                        }
+                    }
+                }
+                CalculateHashes = targetnfo.LastWriteTimeUtc < youngestOne.LastWriteTimeUtc;
             }
         }
     }
---
+++ DjvuNet.Tests/UtilJsonTests.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace DjvuNet.Tests
+{
+    public class UtilJsonTests : IDisposable
+    {
+        string ArtifactsDirectory = "artifacts"; 
+        string ArtifactsSearchPattern = "*.djvu;*.json";
+        string ArtifactsDjvuFile = "test001C.djvu";
+        string ArtifactsJsonFile = "test001C.json";
+        string SubdirectoryList = "data";
+        string SubdirectorySearchPatterns = "*";
+        string HashFilePath = "artifacts.SHA256";
+        string DataDirectory = "data";
+        string DataDirectoryFile = "textest.bz";
+        bool CalculateHashes = false;
+
+        string TemporaryFolder;
+
+        public UtilJsonTests()
+        {
+            TemporaryFolder = Path.Combine(Path.GetTempPath(), "ArtifactsTmpTest");
+            if (Directory.Exists(TemporaryFolder))
+                Directory.Delete(TemporaryFolder);
+            Directory.CreateDirectory(TemporaryFolder);
+            ArtifactsDirectory = Path.Combine(TemporaryFolder, ArtifactsDirectory);
+            Directory.CreateDirectory(ArtifactsDirectory);
+            using (var file = File.Create(Path.Combine(ArtifactsDirectory, ArtifactsDjvuFile)))
+                file.WriteByte(0x42);
+            using (var file = File.Create(Path.Combine(ArtifactsDirectory, ArtifactsJsonFile)))
+                file.WriteByte(0x41);
+            DataDirectory = Path.Combine(ArtifactsDirectory, DataDirectory);
+            Directory.CreateDirectory(DataDirectory);
+            using (var file = File.Create(Path.Combine(DataDirectory, DataDirectoryFile)))
+                file.WriteByte(0x42);
+
+            using (FileStream file = File.Create(Path.Combine(TemporaryFolder, HashFilePath)))
+                file.WriteByte(0x42);
+        }
+
+        public void Dispose()
+        {
+            Directory.Delete(TemporaryFolder, true);
+        }
+
+        [Fact]
+        public void BuildRequiredTest001() 
+        {
+            FileInfo hashFileInfo = new FileInfo(HashFilePath);
+            if (hashFileInfo.Exists)
+            {
+                string backupHashFile = HashFilePath + ".bak";
+                if (File.Exists(backupHashFile))
+                    File.Delete(backupHashFile);
+                try
+                {
+                    hashFileInfo.MoveTo(backupHashFile);
+                    CalculateHashes = false;
+                    UtilJson.ArtifactsBuildRequired(ArtifactsDirectory, ArtifactsSearchPattern, SubdirectoryList,
+                        SubdirectorySearchPatterns, HashFilePath, out CalculateHashes);
+                    Assert.True(CalculateHashes);
+                }
+                finally
+                {
+                    if (File.Exists(backupHashFile))
+                    {
+                        if (!File.Exists(HashFilePath))
+                            File.Move(backupHashFile, HashFilePath);
+                        else
+                            File.Delete(backupHashFile);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public void BuildRequiredTest002()
+        {
+            FileInfo hashFileInfo = new FileInfo(HashFilePath);
+            if (hashFileInfo.Exists)
+            {
+                DateTime prevLastWriteUtc = hashFileInfo.LastWriteTimeUtc;
+                FileInfo test001CInfo = new FileInfo(Path.Combine(ArtifactsDirectory, ArtifactsDjvuFile));
+                DateTime prevTestLastWriteUtc = test001CInfo.LastWriteTimeUtc;
+                try
+                {
+                    test001CInfo.LastWriteTimeUtc = DateTime.UtcNow.AddDays(10);
+                    CalculateHashes = false;
+                    UtilJson.ArtifactsBuildRequired(ArtifactsDirectory, ArtifactsSearchPattern, SubdirectoryList,
+                        SubdirectorySearchPatterns, HashFilePath, out CalculateHashes);
+
+                    Assert.True(CalculateHashes);
+
+                    CalculateHashes = false;
+                    hashFileInfo.LastWriteTimeUtc = test001CInfo.LastWriteTimeUtc.AddDays(10);
+                    UtilJson.ArtifactsBuildRequired(ArtifactsDirectory, ArtifactsSearchPattern, SubdirectoryList,
+                        SubdirectorySearchPatterns, HashFilePath, out CalculateHashes);
+
+                    Assert.False(CalculateHashes);
+                }
+                finally
+                {
+                    hashFileInfo.LastWriteTimeUtc = prevLastWriteUtc;
+                    test001CInfo.LastWriteTimeUtc = prevTestLastWriteUtc;
+                }
+            }
+        }
+
+        [Fact]
+        public void BuildRequiredTest003()
+        {
+            FileInfo hashFileInfo = new FileInfo(HashFilePath);
+            if (hashFileInfo.Exists)
+            {
+                DateTime prevLastWriteUtc = hashFileInfo.LastWriteTimeUtc;
+                FileInfo test001CInfo = new FileInfo(Path.Combine(DataDirectory, "test001C.djvu"));
+                DateTime prevTestLastWriteUtc = test001CInfo.LastWriteTimeUtc;
+                try
+                {
+                    test001CInfo.LastWriteTimeUtc = DateTime.UtcNow.AddDays(10);
+                    CalculateHashes = false;
+                    UtilJson.ArtifactsBuildRequired(ArtifactsDirectory, ArtifactsSearchPattern, SubdirectoryList,
+                        SubdirectorySearchPatterns, HashFilePath, out CalculateHashes);
+
+                    Assert.True(CalculateHashes);
+
+                    CalculateHashes = false;
+                    hashFileInfo.LastWriteTimeUtc = test001CInfo.LastWriteTimeUtc.AddDays(10);
+                    UtilJson.ArtifactsBuildRequired(ArtifactsDirectory, ArtifactsSearchPattern, SubdirectoryList,
+                        SubdirectorySearchPatterns, HashFilePath, out CalculateHashes);
+
+                    Assert.False(CalculateHashes);
+                }
+                finally
+                {
+                    hashFileInfo.LastWriteTimeUtc = prevLastWriteUtc;
+                    test001CInfo.LastWriteTimeUtc = prevTestLastWriteUtc;
+                }
+            }
+        }
+    }
+}
