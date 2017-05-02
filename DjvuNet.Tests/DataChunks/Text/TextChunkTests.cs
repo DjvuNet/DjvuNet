cbe760c173ff5bec3add4fcce21bbe5301a12a3e
Jacek Blaszczynski <jacek.blaszczynski@outlook.com>
2017-05-02 02:59:23 +0200

    Enable solution file independent build consistency for project files. It allows to include DjvuNet projects into other solutions while still present in repo tree without breaking build.

---
+++ DjvuNet.Tests/DataChunks/Text/TextChunkTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using DjvuNet.DataChunks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DjvuNet.DataChunks.Tests
+{
+    public class TextChunkTests
+    {
+        [Fact()]
+        public void TextChunkTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ReadDataTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
