93e711eef4c07ce6ffb1ab6c1485bb2ad25b141f
Jacek Blaszczynski <biosciencenow@outlook.com>
2017-05-15 19:03:26 +0200

    Start Skia backend implementation

---
+++ DjvuNet.Skia/DjvuSkiaGraphics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+
+namespace DjvuNet.Skia
+{
+    public class DjvuSkiaGraphics
+    {
+        public static byte[] GetImageData(string file)
+        {
+            using (var bmp = SKBitmap.Decode(file))
+                return bmp.Bytes;
+        }
+    }
+}
