DjvuNet/DjvuNet

commit e26512ded945634d3cdd56dcda71f1f4827cd93b

Refactoring of DjvuNet.Wavelet namespace

DjvuNet/Wavelet/InterWaveBlock.cs
@@ -8,7 +8,7 @@ namespace DjvuNet.Wavelet
     /// This class represents structured wavelet data.
     /// </summary>
     [StructLayout(LayoutKind.Sequential)]
-    public class InterWaveBlock : IInterWaveBlock
+    public unsafe class InterWaveBlock : IInterWaveBlock
     {
         #region Fields
 
@@ -17,7 +17,7 @@ namespace DjvuNet.Wavelet
         /// </summary>
         internal short[][][] _PData;
 
-        internal readonly short[] _Zigzagloc;
+        internal static short[] _Zigzagloc;
 
         #endregion Fields
 
@@ -36,7 +36,8 @@ namespace DjvuNet.Wavelet
         public InterWaveBlock()
         {
             _PData = new short[4][][];
-            _Zigzagloc = BuildZigZagData();
+            if (_Zigzagloc == null)
+                _Zigzagloc = BuildZigZagData();
         }
 
         #endregion Constructors
@@ -155,10 +156,10 @@ namespace DjvuNet.Wavelet
         }
 
         /// <summary> 
-        /// Write a liftblock
+        /// Write a liftblock. Implementation assumes all coeff buffer values are zeros.
         /// </summary>
         /// <param name="coeff">
-        /// An array of block wavelet coefficients
+        /// An array of block wavelet coefficients - function assumes all values are zeros
         /// </param>
         /// <param name="bmin">
         /// Start position
@@ -169,9 +170,6 @@ namespace DjvuNet.Wavelet
         public void WriteLiftBlock(short[] coeff, int bmin, int bmax)
         {
             int n = bmin << 4;
-
-            for (int i = 0; i < coeff.Length; i++)
-                coeff[i] = 0;
 
             for (int n1 = bmin; n1 < bmax; n1++)
             {
@@ -191,7 +189,7 @@ namespace DjvuNet.Wavelet
             }
         }
 
-        public void read_liftblock(short[] coeff)
+        public void ReadLiftBlock(short[] coeff)
         {
             int n = 0;
             for (int n1 = 0; n1 < 64; n1++)

DjvuNet.Tests/Parser/DjvuParserTests.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using DjvuNet.DataChunks;
 using Moq;
 using DjvuNet.Tests.Xunit;
+using DjvuNet.Errors;
 
 namespace DjvuNet.Parser.Tests
 {
@@ -52,6 +53,45 @@ namespace DjvuNet.Parser.Tests
             }
         }
 
+        public static IEnumerable<object[]> EncodedNodeTestData
+        {
+            get
+            {
+                List<object[]> retVal = new List<object[]>();
+
+                retVal.Add(new object[] { null, null, ChunkType.Anta, 1 });
+                retVal.Add(new object[] { null, null, ChunkType.Antz, 2 });
+                retVal.Add(new object[] { null, null, ChunkType.BG44, 3 });
+                retVal.Add(new object[] { null, null, ChunkType.BGjp, 4 });
+                retVal.Add(new object[] { null, null, ChunkType.Cida, 5 });
+                retVal.Add(new object[] { null, null, ChunkType.Dirm, 6 });
+                retVal.Add(new object[] { null, null, ChunkType.Djbz, 7 });
+                retVal.Add(new object[] { null, null, ChunkType.Djvi, 8 });
+                retVal.Add(new object[] { null, null, ChunkType.Djvm, 9 });
+                retVal.Add(new object[] { null, null, ChunkType.Djvu, 10 });
+                retVal.Add(new object[] { null, null, ChunkType.FG44, 11 });
+                retVal.Add(new object[] { null, null, ChunkType.FGbz, 12 });
+                retVal.Add(new object[] { null, null, ChunkType.FGjp, 13 });
+                retVal.Add(new object[] { null, null, ChunkType.Incl, 14 });
+                retVal.Add(new object[] { null, null, ChunkType.Info, 15 });
+                retVal.Add(new object[] { null, null, ChunkType.Navm, 16 });
+                retVal.Add(new object[] { null, null, ChunkType.Sjbz, 17 });
+                retVal.Add(new object[] { null, null, ChunkType.Smmr, 18 });
+                retVal.Add(new object[] { null, null, ChunkType.TH44, 19 });
+                retVal.Add(new object[] { null, null, ChunkType.Thum, 20 });
+                retVal.Add(new object[] { null, null, ChunkType.Txta, 21 });
+                retVal.Add(new object[] { null, null, ChunkType.Txtz, 22 });
+                retVal.Add(new object[] { null, null, ChunkType.Wmrm, 23 });
+                retVal.Add(new object[] { null, null, ChunkType.BM44, 24 });
+                retVal.Add(new object[] { null, null, ChunkType.PM44, 25 });
+                retVal.Add(new object[] { null, null, ChunkType.BM44Form, 26 });
+                retVal.Add(new object[] { null, null, ChunkType.PM44Form, 27 });
+                retVal.Add(new object[] { null, null, ChunkType.Unknown, 28 });
+
+                return retVal;
+            }
+        }
+
         public static IEnumerable<object[]> RootFormChildTestData
         {
             get
@@ -70,7 +110,7 @@ namespace DjvuNet.Parser.Tests
 
         [DjvuTheory]
         [MemberData(nameof(NodeTestData))]
-        public void CreateDjvuNode_Theory(IDjvuReader reader, IDjvuDocument rootDocument,
+        public void CreateDecodedDjvuNode_Theory(IDjvuReader reader, IDjvuDocument rootDocument,
             IDjvuElement parent, ChunkType chunkType,
             string chunkID = "", long length = 0)
         {
@@ -86,7 +126,7 @@ namespace DjvuNet.Parser.Tests
             Assert.Equal<ChunkType>(chunkType, node.ChunkType);
             Assert.Equal<string>(chunkID, node.ChunkID);
             Assert.Equal<long>(length, node.Length);
-            Assert.Equal<long>(reader.Position, node.DataOffset); 
+            Assert.Equal<long>(reader.Position, node.DataOffset);
         }
 
         [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
@@ -169,5 +209,31 @@ namespace DjvuNet.Parser.Tests
             Assert.False(DjvuParser.IsRootFormChild(ChunkType.Wmrm));
         }
 
+        [DjvuTheory]
+        [MemberData(nameof(EncodedNodeTestData))]
+        public void CreateEncodedDjvuNode_Theory(IDjvuWriter writer, IDjvuElement parent, ChunkType chunkType, long length = 0)
+        {
+            if (writer == null)
+            {
+                Mock<IDjvuWriter> writerMock = new Mock<IDjvuWriter>();
+                writerMock.Setup<long>(x => x.Position).Returns(1024);
+                writer = writerMock.Object;
+            }
+
+            switch (chunkType)
+            {
+                case ChunkType.Cida:
+                case ChunkType.Unknown:
+                    Assert.Throws<DjvuInvalidOperationException>(
+                        () => DjvuParser.CreateEncodedDjvuNode(writer, parent, chunkType, length));
+                    break;
+                default:
+                    IDjvuNode node = DjvuParser.CreateEncodedDjvuNode(writer, parent, chunkType, length);
+                    Assert.NotNull(node);
+                    Assert.Equal<ChunkType>(chunkType, node.ChunkType);
+                    Assert.Equal<long>(length, node.Length);
+                    break;
+            }
+        }
     }
 }

DjvuNet.Wavelet.Tests/DjvuNet.Wavelet.Tests.csproj
@@ -154,11 +154,13 @@
     <Compile Include="Properties\AssemblyInfoCommon.cs" />
     <Compile Include="Util.cs" />
     <Compile Include="UtilJsonTests.cs" />
+    <Compile Include="Wavelet\InterWaveTransformTests.cs" />
     <Content Include="Properties\AssemblyInfo.Template.cs" />
     <Compile Include="Properties\AssemblyInfo.cs" />
     <Compile Include="Wavelet\InterWaveBlockTests.cs" />
     <Compile Include="Wavelet\InterWaveBucketTests.cs" />
     <Compile Include="Wavelet\InterWaveCodecTests.cs" />
+    <Compile Include="Wavelet\InterWaveEncoderTests.cs" />
     <Compile Include="Wavelet\InterWaveMapTests.cs" />
     <Compile Include="Wavelet\InterWavePixelMapTests.cs" />
   </ItemGroup>

DjvuNet.Wavelet.Tests/Wavelet/InterWaveBlockTests.cs
@@ -32,7 +32,7 @@ namespace DjvuNet.Wavelet.Tests
                     short[] pdataij = pdata[i][j];
 
                     for (int k = 0; k < pdataij.Length; k++)
-                        pdataij[k] = unchecked((short)(((i * 256) + (j * 16) + k ) * growthSlope + baseVal));
+                        pdataij[k] = unchecked((short)(((i * 256) + (j * 16) + k) * growthSlope + baseVal));
                 }
             }
         }
@@ -42,7 +42,7 @@ namespace DjvuNet.Wavelet.Tests
         {
             InterWaveBlock block = new InterWaveBlock();
             Assert.NotNull(block._PData);
-            Assert.NotNull(block._Zigzagloc);
+            Assert.NotNull(InterWaveBlock._Zigzagloc);
         }
 
         [Fact()]
@@ -50,7 +50,7 @@ namespace DjvuNet.Wavelet.Tests
         {
             InterWaveBlock block = new InterWaveBlock();
             Assert.NotNull(block._PData);
-            Assert.NotNull(block._Zigzagloc);
+            Assert.NotNull(InterWaveBlock._Zigzagloc);
             block._PData = null;
 
             var block2 = block.Duplicate();
@@ -62,7 +62,7 @@ namespace DjvuNet.Wavelet.Tests
         {
             InterWaveBlock block = new InterWaveBlock();
             Assert.NotNull(block._PData);
-            Assert.NotNull(block._Zigzagloc);
+            Assert.NotNull(InterWaveBlock._Zigzagloc);
 
             short[][][] pdata = block._PData;
 
@@ -355,5 +355,36 @@ namespace DjvuNet.Wavelet.Tests
             for (int i = 0; i < testData.Length; i++)
                 Assert.Equal(testData[i], zigZag[i]);
         }
+
+        [Fact()]
+        public void PDataTest()
+        {
+            InterWaveBlock block = new InterWaveBlock();
+            short[][][] pdata = block.PData;
+            Assert.NotNull(pdata);
+            Assert.Equal(4, pdata.Length);
+
+            short[][][] pdata2 = new short[8][][];
+            block.PData = pdata2;
+
+            Assert.NotSame(pdata, pdata2);
+
+            pdata = block.PData;
+            Assert.Same(pdata, pdata2);
+        }
+
+        [Fact()]
+        public void ReadLiftBlockTest()
+        {
+            InterWaveBlock block = new InterWaveBlock();
+            short[] coeff = new short[1024];
+
+            for (short i = 0; i < coeff.Length; i++)
+                coeff[i] = (short)(i * 3 % 793);
+
+            block.ReadLiftBlock(coeff);
+        }
+
+
     }
 }

DjvuNet.Wavelet.Tests/Wavelet/InterWaveEncoderTests.cs
@@ -1,0 +1,61 @@
+﻿using Xunit;
+using DjvuNet.Wavelet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DjvuNet.Wavelet.Tests
+{
+    public class InterWaveEncoderTests
+    {
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void InterWaveEncoderTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void CodeSliceTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void IsNullSliceTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void EncodeBucketsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void FinishCodeSliceTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void EstimateDecibelTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void InterpolateMaskTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void ForwardMaskTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}

DjvuNet.Wavelet.Tests/Wavelet/InterWaveMapTests.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.IO;
 using DjvuNet.Tests;
 using DjvuNet.Graphics;
+using DjvuNet.Errors;
 
 namespace DjvuNet.Wavelet.Tests
 {
@@ -15,12 +16,6 @@ namespace DjvuNet.Wavelet.Tests
     {
         [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
         public void InterWaveMapTest()
-        {
-            Assert.True(false, "This test needs an implementation");
-        }
-
-        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
-        public void IWMapTest1()
         {
             Assert.True(false, "This test needs an implementation");
         }
@@ -35,11 +30,11 @@ namespace DjvuNet.Wavelet.Tests
                 InterWavePixelMap map = new InterWavePixelMap();
                 map.Decode(reader);
                 Assert.NotNull(map._YMap);
-                Assert.NotNull(map._YCodec);
+                Assert.NotNull(map._YDecoder);
                 Assert.NotNull(map._CbMap);
-                Assert.NotNull(map._CbCodec);
+                Assert.NotNull(map._CbDecoder);
                 Assert.NotNull(map._CrMap);
-                Assert.NotNull(map._CrCodec);
+                Assert.NotNull(map._CrDecoder);
 
                 var dyMap = map._YMap.Duplicate();
                 Assert.NotNull(dyMap);
@@ -59,11 +54,11 @@ namespace DjvuNet.Wavelet.Tests
                 InterWavePixelMap map = new InterWavePixelMap();
                 map.Decode(reader);
                 Assert.NotNull(map._YMap);
-                Assert.NotNull(map._YCodec);
+                Assert.NotNull(map._YDecoder);
                 Assert.NotNull(map._CbMap);
-                Assert.NotNull(map._CbCodec);
+                Assert.NotNull(map._CbDecoder);
                 Assert.NotNull(map._CrMap);
-                Assert.NotNull(map._CrCodec);
+                Assert.NotNull(map._CrDecoder);
 
                 map._YMap.Blocks = null;
                 var dyMap = map._YMap.Duplicate();
@@ -101,15 +96,14 @@ namespace DjvuNet.Wavelet.Tests
                 InterWavePixelMap map = new InterWavePixelMap();
                 map.Decode(reader);
                 Assert.NotNull(map._YMap);
-                Assert.NotNull(map._YCodec);
+                Assert.NotNull(map._YDecoder);
                 Assert.NotNull(map._CbMap);
-                Assert.NotNull(map._CbCodec);
+                Assert.NotNull(map._CbDecoder);
                 Assert.NotNull(map._CrMap);
-                Assert.NotNull(map._CrCodec);
+                Assert.NotNull(map._CrDecoder);
 
                 int result = map._YMap.GetBucketCount();
                 Assert.Equal(910, result);
-
             }
         }
 
@@ -127,9 +121,8 @@ namespace DjvuNet.Wavelet.Tests
                 Top = height
             };
 
-            InterWaveMap map = new InterWaveMap();
-            map.Init(width, height);
-            Assert.Throws<ArgumentOutOfRangeException>(
+            InterWaveMap map = new InterWaveMap(width, height);
+            Assert.Throws<DjvuArgumentOutOfRangeException>(
                 () => map.Image(subsample, rect, 1, new sbyte[width * height], width, 1, true));
         }
 
@@ -147,9 +140,8 @@ namespace DjvuNet.Wavelet.Tests
                 Top = height
             };
 
-            InterWaveMap map = new InterWaveMap();
-            map.Init(width, height);
-            Assert.Throws<ArgumentException>(
+            InterWaveMap map = new InterWaveMap(width, height);
+            Assert.Throws<DjvuArgumentException>(
                 () => map.Image(subsample, rect, 1, new sbyte[width * height], width, 1, true));
         }
 
@@ -168,9 +160,8 @@ namespace DjvuNet.Wavelet.Tests
                 Top = height
             };
 
-            InterWaveMap map = new InterWaveMap();
-            map.Init(width, height);
-            Assert.Throws<ArgumentException>(
+            InterWaveMap map = new InterWaveMap(width, height);
+            Assert.Throws<DjvuArgumentException>(
                 () => map.Image(subsample, rect, 1, new sbyte[width * height], width, 1, true));
         }
 
@@ -190,9 +181,8 @@ namespace DjvuNet.Wavelet.Tests
                 Top = height - 3
             };
 
-            InterWaveMap map = new InterWaveMap();
-            map.Init(width, height);
-            Assert.Throws<ArgumentException>(
+            InterWaveMap map = new InterWaveMap(width, height);
+            Assert.Throws<DjvuArgumentException>(
                 () => map.Image(subsample, rect, 1, new sbyte[width * height], width, 1, true));
         }
 
@@ -210,8 +200,7 @@ namespace DjvuNet.Wavelet.Tests
                 Top = height
             };
 
-            InterWaveMap map = new InterWaveMap();
-            map.Init(width, height);
+            InterWaveMap map = new InterWaveMap(width, height);
             map.Image(0, new sbyte[width * height], width, 1, true);
         }
 
@@ -230,8 +219,7 @@ namespace DjvuNet.Wavelet.Tests
                 Top = height
             };
 
-            InterWaveMap map = new InterWaveMap();
-            map.Init(width, height);
+            InterWaveMap map = new InterWaveMap(width, height);
             map.Image(subsample, rect, 1, new sbyte[width * height * 3], width, 1, true);
         }
 
@@ -250,15 +238,8 @@ namespace DjvuNet.Wavelet.Tests
                 Top = height
             };
 
-            InterWaveMap map = new InterWaveMap();
-            map.Init(width, height);
+            InterWaveMap map = new InterWaveMap(width, height);
             map.Image(subsample, rect, 1, new sbyte[width * height + 1], width, 1, true);
-        }
-
-        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
-        public void InitTest()
-        {
-            Assert.True(false, "This test needs an implementation");
         }
 
         [Fact()]
@@ -271,11 +252,11 @@ namespace DjvuNet.Wavelet.Tests
                 InterWavePixelMap map = new InterWavePixelMap();
                 map.Decode(reader);
                 Assert.NotNull(map._YMap);
-                Assert.NotNull(map._YCodec);
+                Assert.NotNull(map._YDecoder);
                 Assert.NotNull(map._CbMap);
-                Assert.NotNull(map._CbCodec);
+                Assert.NotNull(map._CbDecoder);
                 Assert.NotNull(map._CrMap);
-                Assert.NotNull(map._CrCodec);
+                Assert.NotNull(map._CrDecoder);
 
                 map._YMap.Slashres(1);
             }
@@ -291,11 +272,11 @@ namespace DjvuNet.Wavelet.Tests
                 InterWavePixelMap map = new InterWavePixelMap();
                 map.Decode(reader);
                 Assert.NotNull(map._YMap);
-                Assert.NotNull(map._YCodec);
+                Assert.NotNull(map._YDecoder);
                 Assert.NotNull(map._CbMap);
-                Assert.NotNull(map._CbCodec);
+                Assert.NotNull(map._CbDecoder);
                 Assert.NotNull(map._CrMap);
-                Assert.NotNull(map._CrCodec);
+                Assert.NotNull(map._CrDecoder);
 
                 map._YMap.Slashres(2);
             }
@@ -311,11 +292,11 @@ namespace DjvuNet.Wavelet.Tests
                 InterWavePixelMap map = new InterWavePixelMap();
                 map.Decode(reader);
                 Assert.NotNull(map._YMap);
-                Assert.NotNull(map._YCodec);
+                Assert.NotNull(map._YDecoder);
                 Assert.NotNull(map._CbMap);
-                Assert.NotNull(map._CbCodec);
+                Assert.NotNull(map._CbDecoder);
                 Assert.NotNull(map._CrMap);
-                Assert.NotNull(map._CrCodec);
+                Assert.NotNull(map._CrDecoder);
 
                 map._YMap.Slashres(4);
             }
@@ -331,14 +312,20 @@ namespace DjvuNet.Wavelet.Tests
                 InterWavePixelMap map = new InterWavePixelMap();
                 map.Decode(reader);
                 Assert.NotNull(map._YMap);
-                Assert.NotNull(map._YCodec);
+                Assert.NotNull(map._YDecoder);
                 Assert.NotNull(map._CbMap);
-                Assert.NotNull(map._CbCodec);
+                Assert.NotNull(map._CbDecoder);
                 Assert.NotNull(map._CrMap);
-                Assert.NotNull(map._CrCodec);
+                Assert.NotNull(map._CrDecoder);
 
                 map._YMap.Slashres(8);
             }
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void CreateTest()
+        {
+            Assert.True(false, "This test needs an implementation");
         }
     }
 }

DjvuNet.Wavelet.Tests/Wavelet/InterWavePixelMapTests.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using DjvuNet.Tests;
+using DjvuNet.Graphics.Tests;
+using DjvuNet.Graphics;
 
 namespace DjvuNet.Wavelet.Tests
 {
@@ -28,11 +30,11 @@ namespace DjvuNet.Wavelet.Tests
                 InterWavePixelMap map = new InterWavePixelMap();
                 map.Decode(reader);
                 Assert.NotNull(map._YMap);
-                Assert.NotNull(map._YCodec);
+                Assert.NotNull(map._YDecoder);
                 Assert.NotNull(map._CbMap);
-                Assert.NotNull(map._CbCodec);
+                Assert.NotNull(map._CbDecoder);
                 Assert.NotNull(map._CrMap);
-                Assert.NotNull(map._CrCodec);
+                Assert.NotNull(map._CrDecoder);
             }
         }
 
@@ -82,19 +84,19 @@ namespace DjvuNet.Wavelet.Tests
                 InterWavePixelMap map = new InterWavePixelMap();
                 map.Decode(reader);
                 Assert.NotNull(map._YMap);
-                Assert.NotNull(map._YCodec);
+                Assert.NotNull(map._YDecoder);
                 Assert.NotNull(map._CbMap);
-                Assert.NotNull(map._CbCodec);
+                Assert.NotNull(map._CbDecoder);
                 Assert.NotNull(map._CrMap);
-                Assert.NotNull(map._CrCodec);
+                Assert.NotNull(map._CrDecoder);
 
                 map.CloseCodec();
                 Assert.NotNull(map._YMap);
-                Assert.Null(map._YCodec);
+                Assert.Null(map._YDecoder);
                 Assert.NotNull(map._CbMap);
-                Assert.Null(map._CbCodec);
+                Assert.Null(map._CbDecoder);
                 Assert.NotNull(map._CrMap);
-                Assert.Null(map._CrCodec);
+                Assert.Null(map._CrDecoder);
             }
         }
 
@@ -115,6 +117,34 @@ namespace DjvuNet.Wavelet.Tests
         {
             InterWavePixelMap map = new InterWavePixelMap();
             Assert.True(map.ImageData);
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void EncodeChunkTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void EncodeImageTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void CloseEncoderTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void InitializeEncoderTest()
+        {
+            int width = 512;
+            int height = 256;
+            var pixMap = PixelMapTests.CreateInitVerifyPixelMap(width, height, Pixel.WhitePixel);
+            InterWavePixelMap map = new InterWavePixelMap();
+            map.InitializeEncoder(pixMap);
         }
     }
 }

DjvuNet.Wavelet.Tests/Wavelet/InterWaveTransformTests.cs
@@ -1,0 +1,67 @@
+﻿using Xunit;
+using DjvuNet.Wavelet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DjvuNet.Wavelet.Tests
+{
+    public class InterWaveTransformTests
+    {
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void ForwardTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void BackwardTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void Rgb2YTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void Rgb2CbTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void Rgb2CrTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void FilterFvTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void FilterFhTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void FilterBvTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
+        public void FilterBhTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}

commit 57efeed945486694c8a8b395cf64c6044af28797

Fix IW44 codec block image propagation bug introduced with commit e26512d

DjvuNet/Wavelet/InterWaveBlock.cs
@@ -171,6 +171,9 @@ namespace DjvuNet.Wavelet
         {
             int n = bmin << 4;
 
+            for (int i = 0; i < coeff.Length; i++)
+                coeff[i] = 0;
+
             for (int n1 = bmin; n1 < bmax; n1++)
             {
                 short[] d = GetBlock(n1);