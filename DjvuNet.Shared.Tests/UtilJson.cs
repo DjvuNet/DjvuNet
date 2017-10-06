d798992bf4314f7a58117b5ea33e72a6f5bab3ea
Jacek Blaszczynski <biosciencenow@outlook.com>
2017-10-06 20:46:07 +0200

    Check test json docs sorting impact

--- DjvuNet.Shared.Tests/UtilJson.cs
+++ DjvuNet.Shared.Tests/UtilJson.cs
@@ -24,6 +24,7 @@ namespace DjvuNet.Tests
                     _JsonDocumentList = new List<DjvuJsonDocument>();
                     string artifactsPath = Path.Combine(Util.RepoRoot, "artifacts");
                     string[] jsonFiles = Directory.GetFiles(Util.ArtifactsPath, "*C*.json");
+                    Array.Sort(jsonFiles);
 
                     foreach (string f in jsonFiles)
                     {
