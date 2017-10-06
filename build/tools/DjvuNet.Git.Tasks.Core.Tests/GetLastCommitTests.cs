507ed9f9d749b3f187514043e76164dbd409e86a
Jacek Blaszczynski <biosciencenow@outlook.com>
2017-10-06 18:17:03 +0200

    Add DjvuNet.Git.Tasks.Core.Tests project

---
+++ build/tools/DjvuNet.Git.Tasks.Core.Tests/GetLastCommitTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DjvuNet.Git.Tasks.Core.Tests
+{
+    public class GetLastCommitTests
+    {
+        [Fact(Skip = "Not yet implemented."), Trait("Category", "Skip")]
+        public void GetLastCommitHash()
+        {
+
+        }
+    }
+}
