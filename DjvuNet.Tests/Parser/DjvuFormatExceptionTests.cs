8a70f380b110a50aeee1c863f6e988a350596f43
Jacek Blaszczynski <biosciencenow@outlook.com>
2017-10-07 01:15:13 +0200

    Port to Linux, create Linux workflow on Travis CI (#98)

--- DjvuNet.Tests/Parser/DjvuFormatExceptionTests.cs
+++ DjvuNet.Tests/Parser/DjvuFormatExceptionTests.cs
@@ -51,8 +51,8 @@ namespace DjvuNet.Tests
             Assert.Null(exception.InnerException);
         }
 
-        [Fact, Trait("SkipFramework", "Core")]
-        public void DjvuFormatExceptionTest3()
+        [Fact, SkipOnTargetFramework(TargetFrameworkMonikers.Netcoreapp), Trait("Category", "SkipNetCoreApp")]
+        public void BinarySerializationRoundtripTest()
         {
             string innerMessage = "Invalid test message";
             InvalidOperationException inner = new InvalidOperationException(innerMessage);
--- DjvuNet/Wavelet/NativeMethods.cs
+++ DjvuNet/Wavelet/NativeMethods.cs
@@ -10,17 +10,19 @@ namespace DjvuNet.Wavelet
     {
         public static unsafe void MoveMemory(void* dest, void* src, long length)
         {
-            CopyMemory(dest, src, (uint) length);
+            //CopyMemory(dest, src, (uint) length);
+            Buffer.MemoryCopy(src, dest, length, length);
         }
 
         public static unsafe void MoveMemory(IntPtr dest, IntPtr src, long length)
         {
-            CopyMemory((void*) dest, (void*) src, (uint) length);
+            //CopyMemory((void*) dest, (void*) src, (uint) length);
+            Buffer.MemoryCopy((void*)src, (void*)dest, length, length);
         }
 
         private unsafe delegate void MemCopyDelegate(void* dest, void* src, long length);
 
-        [DllImport("NtDll.dll", EntryPoint = "RtlMoveMemory")]
-        private unsafe static extern void CopyMemory(void* dest, void* src, uint Length);
+        // [DllImport("NtDll.dll", EntryPoint = "RtlMoveMemory")]
+        // private unsafe static extern void CopyMemory(void* dest, void* src, uint Length);
     }
 }
