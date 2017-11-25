79933210efa24bb39fc62badec0857fc6d78fede
Jacek Blaszczynski <biosciencenow@outlook.com>
2017-11-25 16:10:05 +0100

    Improve Map implementations and add tests for new code

--- DjvuNet.Wavelet.Tests/Graphics/MapTests.cs
+++ DjvuNet.Wavelet.Tests/Graphics/MapTests.cs
@@ -15,7 +15,6 @@ namespace DjvuNet.Graphics.Tests
         public void MapTest()
         {
             Mock<Map> mapMock = new Mock<Map> { CallBase = true };
-                
         }
 
         [Fact(Skip = "Not implemented"), Trait("Category", "Skip")]
@@ -66,44 +65,98 @@ namespace DjvuNet.Graphics.Tests
             Assert.True(false, "This test needs an implementation");
         }
 
-        //[Fact()]
-        //public void ToImageTest001()
-        //{
-        //    int width = 32;
-        //    int height = 32;
-        //    Pixel color = Pixel.BluePixel;
+        [Fact(Skip = "Skip due to AVE in GDI+ - Graphics"), Trait("Category", "Skip")]
+        public void ToImageTest001()
+        {
+            int width = 32;
+            int height = 32;
+            Pixel color = Pixel.BluePixel;
 
-        //    IPixelMap map1 = PixelMapTests.CreateInitVerifyPixelMap(width, height, color);
-        //    map1.BytesPerPixel = 2;
-        //    using (System.Drawing.Bitmap bmp = map1.ToImage())
-        //    {
-        //        Assert.NotNull(bmp);
-        //        Assert.IsType<System.Drawing.Bitmap>(bmp);
-        //        Assert.Equal(width, bmp.Width);
-        //        Assert.Equal(height, bmp.Height);
-        //    }
-        //}
+            IPixelMap map1 = PixelMapTests.CreateInitVerifyPixelMap(width, height, color);
+            map1.BytesPerPixel = 1;
+            using (System.Drawing.Bitmap bmp = map1.ToImage())
+            {
+                Assert.NotNull(bmp);
+                Assert.IsType<System.Drawing.Bitmap>(bmp);
+                Assert.Equal(width, bmp.Width);
+                Assert.Equal(height, bmp.Height);
+            }
+        }
 
-        //[Fact()]
-        //public void ToImageTest002()
-        //{
-        //    int width = 32;
-        //    int height = 32;
-        //    Pixel color = Pixel.BluePixel;
+        [Fact(Skip = "Skip due to AVE in GDI+ - Graphics"), Trait("Category", "Skip")]
+        public void ToImageTest002()
+        {
+            int width = 32;
+            int height = 32;
+            Pixel color = Pixel.BluePixel;
 
-        //    IPixelMap map1 = PixelMapTests.CreateInitVerifyPixelMap(width, height, color);
-        //    map1.BytesPerPixel = 3;
-        //    using (System.Drawing.Bitmap bmp = map1.ToImage())
-        //    {
-        //        Assert.NotNull(bmp);
-        //        Assert.IsType<System.Drawing.Bitmap>(bmp);
-        //        Assert.Equal(width, bmp.Width);
-        //        Assert.Equal(height, bmp.Height);
-        //    }
-        //}
+            IPixelMap map1 = PixelMapTests.CreateInitVerifyPixelMap(width, height, color);
+            map1.BytesPerPixel = 2;
+            using (System.Drawing.Bitmap bmp = map1.ToImage())
+            {
+                Assert.NotNull(bmp);
+                Assert.IsType<System.Drawing.Bitmap>(bmp);
+                Assert.Equal(width, bmp.Width);
+                Assert.Equal(height, bmp.Height);
+            }
+        }
+
+        [Fact()]
+        public void ToImageTest008()
+        {
+            int width = 32;
+            int height = 32;
+            Pixel color = Pixel.BluePixel;
+
+            IPixelMap map1 = PixelMapTests.CreateInitVerifyPixelMap(width, height, color);
+            map1.BytesPerPixel = 8;
+            using (System.Drawing.Bitmap bmp = map1.ToImage())
+            {
+                Assert.NotNull(bmp);
+                Assert.IsType<System.Drawing.Bitmap>(bmp);
+                Assert.Equal(width, bmp.Width);
+                Assert.Equal(height, bmp.Height);
+            }
+        }
+
+        [Fact()]
+        public void ToImageTest006()
+        {
+            int width = 32;
+            int height = 32;
+            Pixel color = Pixel.BluePixel;
+
+            IPixelMap map1 = PixelMapTests.CreateInitVerifyPixelMap(width, height, color);
+            map1.BytesPerPixel = 6;
+            using (System.Drawing.Bitmap bmp = map1.ToImage())
+            {
+                Assert.NotNull(bmp);
+                Assert.IsType<System.Drawing.Bitmap>(bmp);
+                Assert.Equal(width, bmp.Width);
+                Assert.Equal(height, bmp.Height);
+            }
+        }
 
         [Fact()]
         public void ToImageTest003()
+        {
+            int width = 32;
+            int height = 32;
+            Pixel color = Pixel.BluePixel;
+
+            IPixelMap map1 = PixelMapTests.CreateInitVerifyPixelMap(width, height, color);
+            map1.BytesPerPixel = 3;
+            using (System.Drawing.Bitmap bmp = map1.ToImage())
+            {
+                Assert.NotNull(bmp);
+                Assert.IsType<System.Drawing.Bitmap>(bmp);
+                Assert.Equal(width, bmp.Width);
+                Assert.Equal(height, bmp.Height);
+            }
+        }
+
+        [Fact()]
+        public void ToImageTest004()
         {
             int width = 32;
             int height = 32;
@@ -121,7 +174,7 @@ namespace DjvuNet.Graphics.Tests
         }
 
         [Fact()]
-        public void ToImageTest004()
+        public void ToImageTest005()
         {
             int width = 32;
             int height = 32;
